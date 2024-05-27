1520b50a6d75f298f26409674e8459552559b741
RaikkoP <raikko.prants@gmail.com>
2024-05-27 23:43:17 +0300

    Understanding basic, made DOM manipulation for demo

--- main.js
+++ main.js
@@ -1,16 +1,33 @@
 const { app, BrowserWindow } = require('electron');
 
+const path = require('node:path');
+
 const createWindow = () => {
-  const win = new BrowserWindow({
+const win = new BrowserWindow({
     width: 800,
-    height: 600
-  });
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+    });
 
-  win.loadFile('index.html');
+  win.loadFile('views/menu.html');
 }
-
 
 app.whenReady().then(() => {
   createWindow();
-})
+
+  // Close on linux and windows...
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+  });  
+
+  // Close and reopen on MAC
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+
+
 
