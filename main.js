7dba6485aa54ea6943ec16b1653819ad849dd29a
RaikkoP <raikko.prants@gmail.com>
2024-05-28 11:14:12 +0300

    Made basic style file for menu, finished font sizes, still needs work with noise effect and buttons

--- main.js
+++ main.js
@@ -3,24 +3,26 @@ const { app, BrowserWindow } = require('electron');
 const path = require('node:path');
 
 const createWindow = () => {
-const win = new BrowserWindow({
+  const win = new BrowserWindow({
     width: 800,
     height: 600,
+    useContentSize: true,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
-    });
+  });
 
   win.loadFile('views/menu.html');
 }
 
+
+
 app.whenReady().then(() => {
   createWindow();
-
   // Close on linux and windows...
   app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') app.quit();
-  });  
+  });
 
   // Close and reopen on MAC
   app.on('activate', () => {
